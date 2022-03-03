5b1c5e14db664840739049da3b2be6d4052bdd84
devarie <schaaparie@gmail.com>
2022-03-03 23:19:41 +0100

    added new project

--- src/cards.js
+++ src/cards.js
@@ -4,45 +4,53 @@ import Vanleersumdranken from './images/Vanleersumdranken.jpeg'
 import freshservice_banner_groot from './images/freshservice_banner_groot.jpeg'
 import Mestudio from './images/Mestudio.jpeg'
 import streetart1 from './images/Streetart1.jpeg'
+import Fashionboat from './images/Fashionboat.PNG'
 
 const cards = [
   {
     id: 1,
+    img: Fashionboat,
+    title: 'Fashionboat',
+    info: 'A React Javascript website, using Prettier, Css with Styled Components', 
+    link: 'https://devarie.github.io/Fashionboat',
+  },
+  {
+    id: 2,
     img: KayakplusBavaria,
     title: 'Kayakexpeditions.nl',
     info: 'A React Javascript website, using Prettier, Eslint and Typescript controlled, image loading due Cloudinary SDK, styling by Material UI',
     link: 'https://devarie.github.io/kayakka/',
   },
   {
-    id: 4,
+    id: 5,
     img: streetart1,
     title: 'This Portfolio',
     info: 'This React.js website is styled by Material UI, using a grid layout, styled with palette colors from Material UI',
     link: '',
   },
   {
-    id: 2,
+    id: 3,
     img: Vanleersumdranken,
     title: 'Vanleersumdranken.nl',
     info: 'Wordpress website built with visual basic, and a Woocommerce integration connected with a Payment Merchant',
     link: 'https://wijnwereld.online/',
   },
   {
-    id: 3,
+    id: 4,
     img: Housebotel,
     title: 'Housebotel.nl',
     info: 'This Wordpress website is built with elementor, has API extern software integrated, and connected with a Payment Merchant',
     link: 'https://housebotel.nl/',
   },
   {
-    id: 5,
+    id: 6,
     img: Mestudio,
     title: 'Mestudio.nl',
     info: 'Basic wordpress blog, backing up, maintenance and migrating to different server ',
     link: 'https://Mestudio.info',
   },
   {
-    id: 6,
+    id: 7,
     img: freshservice_banner_groot,
     title: 'Businessportal',
     info: 'Implemented new styling to a employee portal',
